ff87a042e3e026b2bbb6139d325ed2186eb6aa8d
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-20 03:17:09 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(speak)
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif speak == speak.upcase
     puts "NO, NOT SINCE 1938!"
-  else 
+  else
     puts "HUH?! SPEAK UP, SONNY!"
   end
 end
